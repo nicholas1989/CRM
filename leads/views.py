063e3cc99e837cbba963abbb9f3d2825d44a2788
nicholas1989 <nwokoronicholas@yahoo.com>
2021-09-18 07:36:10 -0700

    New development

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 def lead_list(request):
@@ -11,6 +12,49 @@ def lead_list(request):
     return render(request, "leads/lead_list.html", context)
 
 def lead_detail(request, pk):
-    print(pk)
-    return HttpResponse("Here is the detail view")
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == "POST":
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
+
+
+"""
+This function is only used if you are using the class Form in the forms.py 
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = form.cleaned_data['agent']
+            Lead.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                age = age,
+                agent = agent
+            )
+            return redirect("/leads")
+    context = {
+        "form": form()
+    }
+    return render(request, "leads/lead_create.html", context)
+
+"""
+
+
 
