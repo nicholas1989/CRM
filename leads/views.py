6abee092c4af535ee19c64ecdab68448ffac12c7
nicholas1989 <nwokoronicholas@yahoo.com>
2021-09-22 00:06:28 -0700

    New codes written

--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,9 @@ from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
+def landing_page(request):
+    return render(request, "landing.html")
+
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
@@ -30,9 +33,55 @@ def lead_create(request):
     }
     return render(request, "leads/lead_create.html", context)
 
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
 
 """
-This function is only used if you are using the class Form in the forms.py 
+This function is only used if you are using the class Form that is derived from the forms.py 
+def lead_updat(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadForm()
+    if request.method = "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = form.cleaned_data['agent']
+            lead.first_name = first_name
+            lead.last_name = last_name
+            lead.age = age 
+            lead.agent = agent
+            lead.save()
+            return redirect("/leads")
+        context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, "leads/lead_update.html", context)
+
+
+"""
+
+"""
+This function is only used if you are using the class Form that is derived from the forms.py 
 def lead_create(request):
     form = LeadForm()
     if request.method == "POST":
